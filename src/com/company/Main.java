8c98f0b70b0ca15d72407a7c327903ba40c76197
Yelena Khamitchina <elena_hamitchina@mail.ru>
2016-11-14 20:12:20 +0600

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("first commit");
+        // write your code here
     }
 }
