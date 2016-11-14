2b3e0907a000d67ae6bd2b71184b721cbc01e815
Yelena Khamitchina <elena_hamitchina@mail.ru>
2016-11-14 22:33:08 +0600

    even/ odd numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("first commit");
-        System.out.println("second commit");
-        // write your code here
+        //1е задание
+        int num;
+        System.out.print("Введите число: ");
+
+        Scanner input = new Scanner(System.in);
+        num = input.nextInt();
+
+        if (num % 2 == 0)
+            System.out.print("четное");
+        else
+            System.out.print("нечетное");
     }
 }
