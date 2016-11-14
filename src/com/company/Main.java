f7cdcc11793d643900457e9c4fc4ca38e98ae425
Yelena Khamitchina <elena_hamitchina@mail.ru>
2016-11-14 20:15:22 +0600

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("first commit");
+        System.out.println("second commit");
         // write your code here
     }
 }
